4e2942266f0d31d62b78d88abc2ff0c2df3046b6
non847 <non847@gmail.com>
2020-04-06 19:12:44 +0300

    add samesite=none to the session cookie

--- app.js
+++ app.js
@@ -58,6 +58,7 @@ var sess = {
 if (env === 'production' || env === 'test') {
     app.set('trust proxy', 1);
     sess.cookie.secure = true;
+    sess.cookie.samesite = 'none';
 }
 
 app.use(session(sess));
